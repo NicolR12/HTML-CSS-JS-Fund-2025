f1d2ff261d74886d679667136a0ac4b0694ee72b
Nicoll Ramirez <ramirez.nicoll1206@gmail.com>
2025-06-17 21:37:28 -0400

    Add files via upload

---
+++ GroupAssignment2/Taskmanagementapp/Data/Application.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+
+
+namespace Taskmanagementapp.Data
+{
+    public class Application : DbContext
+    {
+        public Application(DbContextOptions<Application> options) : base(options) { }
+    
+    public DbSet<Application> Applications { get; set; }
+    }
+}
---
+++ GroupAssignment2/Taskmanagementapp/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Taskmanagementapp.Models
+{
+    public class Category
+    {
+        public int categoryID
+        {
+            get; set;
+        }
+        public string Name { get; set; }
+
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
